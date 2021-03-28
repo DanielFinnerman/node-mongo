776019ac8408a5c2bb545089ce6409a834201ae6
Daniel Finnerman <danu95d@hotmail.com>
2021-03-29 00:35:17 +0300

    chargestation2

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const db = require('./db');
+const routes = require('./route')
 
 app.use(express.urlencoded({extended: false}));
-app.use('/station', require('./routes'));
+
+app.use(routes);
 
 db.on('connected', () => {
 	app.listen(3000);
--- model.js
+++ model.js
@@ -2,17 +2,55 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const catSchema = new Schema({
-  name: String,
-  age: {
-	type: Number,
-	min: [0, 'haloo wat doink'],
-	max: [42, 'haloo wake up brother'] 
-  },
-  genre: {
-	  type: String,
-	  enum: ['male', 'female', 'robot']
-  }
+const connectionsSchema = new Schema({
+  ConnectionTypeID: mongoose.ObjectId,
+  LevelID: mongoose.ObjectId,
+  CurrentTypeID: mongoose.ObjectId,
+  Quantity: Number
 });
 
-module.exports = mongoose.model('Cat', catSchema);
+const connectiontypesSchema = new Schema({
+  FormalName: String,
+  Title: String
+})
+
+const currenttypesSchema = new Schema({
+  Description: String,
+  Title: String
+})
+
+const levelsSchema = new Schema({
+  Comments: String,
+  IsFastChargeCapable: Boolean,
+  Title: String
+})
+
+const stationsSchema = new Schema({
+  Location: new Schema({
+    coordinates: [Number],
+    type: String
+  }),
+  Connections: [{type: Schema.ObjectId, ref: 'connections'}],
+  Title: String,
+  AddressLine1: String,
+  Town: String,
+  StateOrProvince: String,
+  Postcode: String
+})
+
+const usersSchema = new Schema({
+  username: String,
+  password: String,
+  full_name: String
+})
+
+const models = {
+  connections: mongoose.model('connections', connectionsSchema),
+  connectiontypes: mongoose.model('connectiontypes', connectiontypesSchema),
+  currenttypes: mongoose.model('currenttypes', currenttypesSchema),
+  levels: mongoose.model('levels', levelsSchema),
+  stations: mongoose.model('stations', stationsSchema),
+  users: mongoose.model('users', usersSchema)
+}
+
+module.exports = models;
