8da5ab5e229f2bdc9887d8e07222deb8f3b8c35e
Daniel Finnerman <danu95d@hotmail.com>
2021-04-03 22:44:10 +0300

    graphql

--- db.js
+++ db.js
@@ -1,12 +1,56 @@
 const mongoose = require('mongoose');
 
-(async () => {
-  try {
-    await mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
-    console.log('DB connected successfully');
-  } catch (err) {
-    console.error('Connection to db failed', err);
-  }
-})();
+const Schema = mongoose.Schema;
 
-module.exports = mongoose.connection;
+const connectionsSchema = new Schema({
+  ConnectionTypeID: mongoose.ObjectId,
+  LevelID: mongoose.ObjectId,
+  CurrentTypeID: mongoose.ObjectId,
+  Quantity: Number
+});
+
+const connectiontypesSchema = new Schema({
+  FormalName: String,
+  Title: String
+})
+
+const currenttypesSchema = new Schema({
+  Description: String,
+  Title: String
+})
+
+const levelsSchema = new Schema({
+  Comments: String,
+  IsFastChargeCapable: Boolean,
+  Title: String
+})
+
+const stationsSchema = new Schema({
+  Location: new Schema({
+    coordinates: [Number],
+    type: String
+  }),
+  Connections: [{type: Schema.ObjectId, ref: 'connections'}],
+  Title: String,
+  AddressLine1: String,
+  Town: String,
+  StateOrProvince: String,
+  Postcode: String
+})
+
+const usersSchema = new Schema({
+  username: String,
+  password: String,
+  full_name: String
+})
+
+const models = {
+  connections: mongoose.model('connections', connectionsSchema),
+  connectiontypes: mongoose.model('connectiontypes', connectiontypesSchema),
+  currenttypes: mongoose.model('currenttypes', currenttypesSchema),
+  levels: mongoose.model('levels', levelsSchema),
+  stations: mongoose.model('stations', stationsSchema),
+  users: mongoose.model('users', usersSchema)
+}
+
+module.exports = models;
